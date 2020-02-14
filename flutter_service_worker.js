5fad6de3ab79814b830f21b25cefab52cccea45e
Rody Davis <rody@rive.app>
2020-02-14 14:06:35 -0800

    Built example/web
    Branch: master
    Commit: 657f9a59595df98c298167b947ca32fe947020b3 (dirty)
    
    package:peanut 3.5.0

---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "b6a04d24d135920059681f55c5b495b6",
+"/main.dart.js": "0db93be4aa0168af7165bc516c4f33ad",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
+"/assets/LICENSE": "d09f1098cef37570917fbce367d5707a",
+"/assets/web/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/assets/web/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/assets/AssetManifest.json": "878b2ddc824cf0efde17fd81a7a36944",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
