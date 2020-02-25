4f8de1f2ab5d32bf95471886b89bb157d68bde1e
Rody Davis <rody@rive.app>
2020-02-25 14:14:52 -0800

    Built example/web
    Branch: master
    Commit: 0b32ae5dc50f042908e9cbf1de0d3b15c8f0a7d3
    
    package:peanut 3.5.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "3df484a5b84f8e23ca72678a0617b195",
-"/main.dart.js": "44b5cabb35626664b448ce30d4c8e5bb",
+"/main.dart.js": "1bd2b4350e964aa182f8d5cd070d1d7b",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
