29db0da985f1eb78ab58590867c8535df56973a7
Rody Davis <rody@rive.app>
2020-02-25 10:36:53 -0800

    updating example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -44,13 +44,98 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
+  bool _isEditing = true;
+
+  bool _exportIos = true;
+  bool _exportWeb = true;
+  bool _exportMacos = true;
+  bool _exportAndroid = true;
+  bool _exportFavicon = true;
+
+  String _iosPath = "ios/Runner/Assets.xcassets/AppIcon.appiconset";
+  String _webPath = "web/icons";
+  String _macOSPath = "macos/Runner/Assets.xcassets/AppIcon.appiconset";
+  String _androidPath = "example/android/app/src/main/res";
+
+  List<IosIcon> _iosIcons = [
+    IosIcon(size: 20, scale: 1),
+    IosIcon(size: 20, scale: 2),
+    IosIcon(size: 20, scale: 3),
+    IosIcon(size: 29, scale: 1),
+    IosIcon(size: 29, scale: 2),
+    IosIcon(size: 29, scale: 3),
+    IosIcon(size: 40, scale: 1),
+    IosIcon(size: 40, scale: 2),
+    IosIcon(size: 40, scale: 3),
+    IosIcon(size: 60, scale: 2),
+    IosIcon(size: 60, scale: 3),
+    IosIcon(size: 76, scale: 1),
+    IosIcon(size: 76, scale: 2),
+    IosIcon(size: 83, scale: 2, point5: true),
+    IosIcon(size: 1024, scale: 1),
+  ];
+
+  List<WebIcon> _webIcons = [
+    WebIcon(size: 192),
+    WebIcon(size: 512),
+  ];
+
+  List<MacOSIcon> _macIcons = [
+    MacOSIcon(size: 16, scale: 1, name: '16'),
+    MacOSIcon(size: 16, scale: 2, name: '32'),
+    MacOSIcon(size: 32, scale: 1, name: '32'),
+    MacOSIcon(size: 32, scale: 2, name: '64'),
+    MacOSIcon(size: 128, scale: 1, name: '128'),
+    MacOSIcon(size: 128, scale: 2, name: '256'),
+    MacOSIcon(size: 256, scale: 1, name: '256'),
+    MacOSIcon(size: 256, scale: 2, name: '512'),
+    MacOSIcon(size: 512, scale: 1, name: '512'),
+    MacOSIcon(size: 512, scale: 2, name: '1024'),
+  ];
+
+  List<AndroidIcon> _androidIcons = [
+    AndroidIcon(size: 72, folderPrefix: "hdpi"),
+    AndroidIcon(size: 48, folderPrefix: "mdpi"),
+    AndroidIcon(size: 96, folderPrefix: "xhdpi"),
+    AndroidIcon(size: 144, folderPrefix: "xxhdpi"),
+    AndroidIcon(size: 192, folderPrefix: "xxxhdpi"),
+  ];
+
+  WebFavicon _webFavicon = WebFavicon();
+
   Future _refresh() async {
     _files = {};
     _setLoading(true);
-    await _generateIcons('iOS Icons', IosIconsFolder());
-    await _generateIcons('Web Icons', WebIconsFolder());
-    await _generateIcons('MacOS Icons', MacOSIconsFolder());
-    await _generateIcons('Android Icons', AndroidIconsFolder());
+    await _generateIcons(
+      'iOS Icons',
+      IosIconsFolder(
+        icons: _iosIcons,
+        path: _iosPath,
+      ),
+    );
+    await _generateIcons(
+      'Web Icons',
+      WebIconsFolder(
+        icons: _webIcons,
+        favicion: _webFavicon,
+        path: _webPath,
+      ),
+    );
+    await _generateIcons(
+      'MacOS Icons',
+      MacOSIconsFolder(
+        icons: _macIcons,
+        path: _macOSPath,
+      ),
+    );
+    await _generateIcons(
+      'Android Icons',
+      AndroidIconsFolder(
+        icons: _androidIcons,
+        path: _androidPath,
+      ),
+    );
+    _isEditing = false;
     _setLoading(false);
   }
 
@@ -148,62 +233,85 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(Icons.archive),
             onPressed: _archive,
           ),
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {
+              if (mounted)
+                setState(() {
+                  if (_isEditing) {
+                    _isEditing = false;
+                    _refresh();
+                  } else {
+                    _isEditing = true;
+                  }
+                });
+            },
+          ),
         ],
       ),
-      body: _files != null
-          ? ListView.separated(
-              separatorBuilder: (context, index) => Container(
-                height: 1.0,
-                color: Colors.grey,
-              ),
-              itemCount: _files.keys.length,
-              itemBuilder: (context, index) {
-                final _key = _files.keys.toList()[index];
-                final _folder = _files[_key];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        _key,
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Wrap(
-                          children: _folder.map((file) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Container(
-                                    height: 200.0,
-                                    width: 200.0,
-                                    child: Card(
-                                      child: Image.memory(
-                                        Uint8List.fromList(file.data),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(file.name),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            )
-          : Center(child: CircularProgressIndicator()),
+      body: _isEditing ? _buildEditingView() : _buildFilePreview(),
       floatingActionButton: FloatingActionButton(
         onPressed: _refresh,
         tooltip: 'Generate Icons',
         child: Icon(Icons.refresh),
       ),
+    );
+  }
+
+  Widget _buildEditingView() {
+    return Container();
+  }
+
+  Widget _buildFilePreview() {
+    if (_files != null) {
+      return Center(child: CircularProgressIndicator());
+    }
+    return ListView.separated(
+      separatorBuilder: (context, index) => Container(
+        height: 1.0,
+        color: Colors.grey,
+      ),
+      itemCount: _files.keys.length,
+      itemBuilder: (context, index) {
+        final _key = _files.keys.toList()[index];
+        final _folder = _files[_key];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                _key,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Wrap(
+                  children: _folder.map((file) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 200.0,
+                            width: 200.0,
+                            child: Card(
+                              child: Image.memory(
+                                Uint8List.fromList(file.data),
+                              ),
+                            ),
+                          ),
+                          Text(file.name),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
